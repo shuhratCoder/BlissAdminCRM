ea7b44fb54b140ed9b1bf79ec8b864dc6df6e185
shuhratCoder <shuhrattuyboyev4@gmail.com>
2026-08-19 09:29:20 +0500

    aa

--- frontend/services/api.ts
+++ frontend/services/api.ts
@@ -1,17 +1,19 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: "/api",
   headers: {
     "Content-Type": "application/json",
   },
 });
 
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
+  if (typeof window !== "undefined") {
+    const token = localStorage.getItem("token");
 
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
   }
 
   return config;
